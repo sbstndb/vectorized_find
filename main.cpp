d1da7a7dbf99be2d7cf4fbe66867d4c2599959f4
sbstndb <sebastiendubois4@gmail.com>
2024-11-16 16:47:04 -0500

    Add Google Benchmark
    	-	Now this run the benchmark
    	-	Result are coming

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 #include <immintrin.h>
 
 
+const int MS = 2 ; // Min_size of arrays
+const int RM = 2 ; /// RangeMultiplier
+const int PS = 12 ; // pow size
+
+
 int naive_find(int* vector, int size, int value){
 	int index = -1 ;
         for (int i = 0 ; i < size ; i++){
@@ -60,6 +65,52 @@ void init_vector(int* vector, int size, int value, int index){
 
 
 
+void BM_NaiveFind(benchmark::State& state){
+	const int size = state.range(0) ; 
+	int* vector = (int*) malloc(sizeof(int) * size) ;
+	int value = 1 ; 
+	init_vector(vector, size, value, size-1);
+	int index ; 
+	for (auto _ : state){
+		index = naive_find(vector, size, value);
+		benchmark::DoNotOptimize(index);
+	}
+	state.SetItemsProcessed(state.iterations() * size);	
+}
+
+void BM_NoBreakFind(benchmark::State& state){
+        const int size = state.range(0) ;
+        int* vector = (int*) malloc(sizeof(int) * size) ;
+        int value = 1 ;
+        init_vector(vector, size, value, size-1);
+        int index ;
+        for (auto _ : state){
+                index = nobreak_find(vector, size, value);
+                benchmark::DoNotOptimize(index);
+        }
+	state.SetItemsProcessed(state.iterations() * size);	
+}
+
+
+void BM_IntrinsicFind(benchmark::State& state){
+        const int size = state.range(0) ;
+	// !! AVX 
+	if (size < 8) {
+		size = 8 ; 
+	}
+        int* vector = (int*) malloc(sizeof(int) * size) ;
+        int value = 1 ;
+        init_vector(vector, size, value, size-1);
+        int index ;
+        for (auto _ : state){
+                index = intrinsic_find(vector, size, value);
+                benchmark::DoNotOptimize(index);
+        }
+	state.SetItemsProcessed(state.iterations() * size);
+}
+
+/**
+
 int main(){
 	int size = 8*256; 
 	int* vector = (int*) malloc(sizeof(int) * size) ; 
@@ -77,7 +128,17 @@ int main(){
 
         int intrinsic_index = intrinsic_find(vector, size, value);
         std::cout << "Index : " << intrinsic_index << std::endl ;
-
-
-
 }
+**/
+
+
+BENCHMARK(BM_NaiveFind)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
+BENCHMARK(BM_NoBreakFind)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
+BENCHMARK(BM_IntrinsicFind)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
+
+
+
+
+BENCHMARK_MAIN() ; 
+
+
