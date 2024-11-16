ae17f61dcf6044f78ad32f7633e24f645ef49ce2
sbstndb <sebastiendubois4@gmail.com>
2024-11-16 16:29:43 -0500

    Add 3 linear find methods
    	-	Naive
    	-	Naive without break : can enable automatic vecto
    	-	intrinsic (works only with avx2 CPU)

---
+++ main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <immintrin.h>
+
+
+int naive_find(int* vector, int size, int value){
+	int index = -1 ;
+        for (int i = 0 ; i < size ; i++){
+                if (vector[i] == value){
+                        index = i ;
+                        return index;
+                }
+        }
+	return index ; 
+}
+
+
+
+int nobreak_find(int* vector, int size, int value){
+        int index = -1 ;
+        for (int i = 0 ; i < size ; i++){
+                if (vector[i] == value){
+                        index = i ;
+			// Note : work if we do not have twice the value. 
+			// If the array is sorted, you can travel the vector 
+			// 	backwards and then get the first value
+                }
+        }
+        return index ;
+}
+
+
+
+
+int intrinsic_find(int* vector, int size, int value){
+	// experimental
+	int index = -1 ; 
+	__m256i target = _mm256_set1_epi32(value) ; 
+
+	for (int i = 0 ; i < size ; i+=8){//avx2
+		__m256i chunk = _mm256_loadu_si256((const __m256i_u*)&vector[i]); // load vector in AVX reg
+		__m256i cmp = _mm256_cmpeq_epi32(chunk, target);
+		int mask_result = _mm256_movemask_epi8(cmp);
+		if (mask_result !=0){
+			int index = __builtin_ctz(mask_result) / 4 ; 
+			return i+index ; 
+		}
+
+	}
+
+
+	return index ;
+}
+
+
+
+
+int main(){
+	int size = 8*256; 
+	int* vector = (int*) malloc(sizeof(int) * size) ; 
+
+	for (int i = 0 ; i < size ; i++){
+		vector[i] = 0 ; 
+	}
+	vector[size - 1] = 1 ; 
+	int value = 1 ; // value to find :-)
+			//
+
+
+	int naive_index = naive_find(vector, size, value);
+	std::cout << "Index : " << naive_index << std::endl ; 
+
+        int nobreak_index = nobreak_find(vector, size, value);
+        std::cout << "Index : " << nobreak_index << std::endl ;
+
+
+        int intrinsic_index = intrinsic_find(vector, size, value);
+        std::cout << "Index : " << intrinsic_index << std::endl ;
+
+
+
+}
